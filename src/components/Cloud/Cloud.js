267a853225fc23fb834c8653a58663a2c374867b
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-05-28 08:26:43 -0700

    Add files via upload

---
+++ src/components/Cloud/Cloud.js
@@ -0,0 +1,17 @@
+import './Cloud.css'
+
+export default function Cloud() {
+  return (
+    <div className='cloud'>
+        <div className="container">
+            <div className="content">
+                <h2><span>Cloud</span> Security</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, eveniet optio! Sint veniam perspiciatis iure odit? Eligendi fuga distinctio ipsum.</p>
+                <div>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
