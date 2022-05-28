67fdb28ef0e44aea62433d51351bdd64395ad88d
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-05-28 08:30:33 -0700

    Add files via upload

---
+++ src/components/Data/Data.js
@@ -0,0 +1,21 @@
+import './Data.css'
+
+export default function Data() {
+  return (
+    <div className='data'>
+
+        <div className="container">
+
+            <div className="content">
+                <h2><span>Data</span> Recovery</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam omnis expedita quis provident repellendus vel, nemo eligendi impedit error voluptatibus.</p>
+                <div>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+
+        </div>
+        
+    </div>
+  )
+}
