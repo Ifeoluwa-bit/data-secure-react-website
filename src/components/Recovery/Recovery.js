f8ddc8d28b50f12a37c762c17d1337d294e42f5b
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-05-28 08:35:41 -0700

    Add files via upload

---
+++ src/components/Recovery/Recovery.js
@@ -0,0 +1,20 @@
+import './Recovery.css'
+
+export default function Recovery() {
+  return (
+    <div className='recovery'>
+        <div className="container">
+
+            <div className="content">
+                <h2><span>Data</span> Recovery</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo rerum, voluptatem cupiditate quibusdam quasi saepe quos sint ducimus sapiente aliquid, illo dicta. Modi alias id saepe quae quasi nemo perferendis.</p>
+                <div>
+                    <button>Learn More</button>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
