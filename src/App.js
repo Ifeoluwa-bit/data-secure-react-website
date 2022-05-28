ee9759a68d44c9c841be4dac86b44817d2ce8f14
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-05-28 08:19:29 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Cloud from "./components/Cloud/Cloud";
+import Data from "./components/Data/Data";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Data />
+      <Cloud />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
